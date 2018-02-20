07d04db3bd9ff5820de1a124fd9f9d8c8099b7f5
Gautineo <gauravsati1996@gmail.com>
2018-02-20 17:15:44 +0530

    Changes done suggested By review team

--- app/src/main/java/com/ncr/interns/codecatchers/incredicabs/Dashboard.java
+++ app/src/main/java/com/ncr/interns/codecatchers/incredicabs/Dashboard.java
@@ -59,13 +59,6 @@ public class Dashboard extends AppCompatActivity
         prepareDashboard();
 
 
-
-
-
-
-
-
-
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
