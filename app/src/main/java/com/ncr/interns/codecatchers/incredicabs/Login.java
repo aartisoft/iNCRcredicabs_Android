1cf19eaee1bfa55aa475a89220e1b7714d15800c
Gautineo <gauravsati1996@gmail.com>
2018-02-22 18:51:11 +0530

    Comiit
    Added the Manager selelction Spinner and Login improvements for Testing

--- app/src/main/java/com/ncr/interns/codecatchers/incredicabs/Login.java
+++ app/src/main/java/com/ncr/interns/codecatchers/incredicabs/Login.java
@@ -20,7 +20,7 @@ import org.json.JSONObject;
 public class Login extends AppCompatActivity {
 
     Button login;
-    EditText user,pass;
+    EditText user, pass;
 
     String url = "http://192.168.0.141:8080/DemoProject/login/doLogin";
 
@@ -29,80 +29,86 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        user=(EditText)findViewById(R.id.editText);
-        pass=(EditText)findViewById(R.id.editText2);
+        user = (EditText) findViewById(R.id.editText);
+        pass = (EditText) findViewById(R.id.editText2);
 
         login = findViewById(R.id.button);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                //json for request
-                JSONObject jsonBodyRequest = new JSONObject();
-                try {
-                    jsonBodyRequest.put("qlid", user.getText().toString());
-                    jsonBodyRequest.put("password", pass.getText().toString());
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-                JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST,
-                        url,
-                        jsonBodyRequest,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-
-                                Log.i("VOLLEY", "inside onResponse method:login");
-                                Log.i("VOLLEY", response.toString());
-
-                                try {
-                                    if (response.getString("status").equalsIgnoreCase("success")) {
-
-                                        String Emp_qlid=response.getString( "Emp_qlid");
-                                        String Mgr_Qlid=response.getString( "Mgr_Qlid");
-                                        String Emp_Address1=response.getString( "Emp_Address1");
-                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Emp_qlid",Emp_qlid).apply();
-                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Mgr_Qlid",Mgr_Qlid).apply();
-                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Emp_Address1",Emp_Address1).apply();
-                                      //  Toast.makeText(Login.this, Emp_qlid+Mgr_Qlid+Emp_Address1, Toast.LENGTH_LONG).show();
-
-                                        Intent intent = new Intent(Login.this,Dashboard.class);
-                                        startActivity(intent);
-                                    } else {
-                                        Toast.makeText(Login.this, "Invalid Credentials", Toast.LENGTH_LONG).show();
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // Do something when error occurred
-                                Log.d("VOLLEY", "Something went wrong");
-                                error.printStackTrace();
-                            }
-                        });
-
-                RESTService.getInstance(Login.this).addToRequestQueue(jsonObjRequest);
-
+                Intent intent = new Intent(Login.this,Dashboard.class);
+                startActivity(intent);
             }
         });
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//                //json for request
+//                JSONObject jsonBodyRequest = new JSONObject();
+//                try {
+//                    jsonBodyRequest.put("qlid", user.getText().toString());
+//                    jsonBodyRequest.put("password", pass.getText().toString());
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//
+//                JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST,
+//                        url,
+//                        jsonBodyRequest,
+//                        new Response.Listener<JSONObject>() {
+//                            @Override
+//                            public void onResponse(JSONObject response) {
+//
+//                                Log.i("VOLLEY", "inside onResponse method:login");
+//                                Log.i("VOLLEY", response.toString());
+//
+//                                try {
+//                                    if (response.getString("status").equalsIgnoreCase("success")) {
+//
+//                                        String Emp_qlid = response.getString("Emp_qlid");
+//                                        String Mgr_Qlid = response.getString("Mgr_Qlid");
+//                                        String Emp_Address1 = response.getString("Emp_Address1");
+//                                        getSharedPreferences(null, MODE_PRIVATE).edit().putString("Emp_qlid", Emp_qlid).apply();
+//                                        getSharedPreferences(null, MODE_PRIVATE).edit().putString("Mgr_Qlid", Mgr_Qlid).apply();
+//                                        getSharedPreferences(null, MODE_PRIVATE).edit().putString("Emp_Address1", Emp_Address1).apply();
+//                                        //  Toast.makeText(Login.this, Emp_qlid+Mgr_Qlid+Emp_Address1, Toast.LENGTH_LONG).show();
+//
+//                                        Intent intent = new Intent(Login.this, Dashboard.class);
+//                                        startActivity(intent);
+//                                    } else {
+//                                        Toast.makeText(Login.this, "Invalid Credentials", Toast.LENGTH_LONG).show();
+//                                    }
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
+//
+//                            }
+//                        },
+//                        new Response.ErrorListener() {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error) {
+//                                // Do something when error occurred
+//                                Log.d("VOLLEY", "Something went wrong");
+//                                error.printStackTrace();
+//                            }
+//                        });
+//
+//                RESTService.getInstance(Login.this).addToRequestQueue(jsonObjRequest);
+//
+//            }
+//        });
 
     }
 
 
-
-@Override
-protected void onStop() {
+    @Override
+    protected void onStop() {
         super.onStop();
         finish();
-        }
-        }
+    }
+}
 
--- app/src/main/java/com/ncr/interns/codecatchers/incredicabs/UnscheduledReqFragment.java
+++ app/src/main/java/com/ncr/interns/codecatchers/incredicabs/UnscheduledReqFragment.java
@@ -40,18 +40,22 @@ import java.util.Locale;
 import static android.content.Context.MODE_PRIVATE;
 
 
-public class UnscheduledReqFragment extends android.support.v4.app.Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+public class UnscheduledReqFragment extends android.support.v4.app.Fragment implements View.OnClickListener{
     int mYear, mMonth, mDay, mHour, mMinute;
-    EditText fromDate, toDate, managerQLid, reasonForRequest, dropLocation, timepicker; //timepickerfrom;
+    EditText fromDate, toDate, reasonForRequest, dropLocation, timepicker; //timepickerfrom;
+    EditText managerQLid_textField;
     Button submit;
     TextView displayLocationSpinner;
     Spinner spinner_location;
+    Spinner managerQLid;
     View rootView;
     CheckBox sat, sun;
     String url = "http://192.168.43.209:8080/DemoProject/req/unscheduled";
     int i = 0;
     String locationArray[] = {"Select", "Home", "Office"};
+    String ApproverArray[] = {"Select", "Lvl 1 Manager", "Lvl2 Manager"};
     NestedScrollView nsv;
+
     public UnscheduledReqFragment() {
         // Required empty public constructor
     }
@@ -64,30 +68,28 @@ public class UnscheduledReqFragment extends android.support.v4.app.Fragment impl
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        //Inflating the xml layout
-        // View rootView = inflater.inflate(R.layout.content_request, container,false);
         View rootView = inflater.inflate(R.layout.content_request, container, false);
 
         //getting the id's
+        managerQLid_textField = rootView.findViewById(R.id.EditText_managerQLID);
         displayLocationSpinner = rootView.findViewById(R.id.display_location_spinner);
         submit = rootView.findViewById(R.id.btn_submit);
         fromDate = rootView.findViewById(R.id.text_fromDate);
         toDate = rootView.findViewById(R.id.text_ToDate);
         timepicker = rootView.findViewById(R.id.time_picker);
-        managerQLid = rootView.findViewById(R.id.text_managerQLID);
         reasonForRequest = rootView.findViewById(R.id.text_reasonForRequest);
         dropLocation = rootView.findViewById(R.id.text_dropLocation);
         nsv = rootView.findViewById(R.id.nestedsv);
-        //  timepickerfrom = rootView.findViewById(R.id.time_pickerfrom);
         sat = rootView.findViewById(R.id.cbsat);
         sun = rootView.findViewById(R.id.cbsun);
-
         /**
          *Code For handling the spinner
          */
 
         spinner_location = rootView.findViewById(R.id.spinner_location);
-        spinner_location.setOnItemSelectedListener(this);
+        managerQLid = rootView.findViewById(R.id.text_managerQLID);
+        spinner_location.setOnItemSelectedListener(new LocationSpinner());
+        managerQLid.setOnItemSelectedListener(new ApprovalManagerQlid());
 
         /**
          * Code for creating the Adapter to add the data of location array into Spinner
@@ -96,17 +98,19 @@ public class UnscheduledReqFragment extends android.support.v4.app.Fragment impl
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_location.setAdapter(adapter);
 
+
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, ApproverArray);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        managerQLid.setAdapter(adapter2);
+
         //added date and time pickers
         fromDate.setOnClickListener(this);
         toDate.setOnClickListener(this);
         timepicker.setOnClickListener(this);
 
-        //timepickerfrom.setOnClickListener(this);
-
-        managerQLid.setText(getActivity().getSharedPreferences(null, MODE_PRIVATE).getString("Mgr_Qlid", "RB250491"));
-
-
-        //Handling the Submit button
+        // managerQLid.setText(getActivity().getSharedPreferences(null, MODE_PRIVATE).getString("Mgr_Qlid", "RB250491"));
+        // Handling the Submit button
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,33 +122,7 @@ public class UnscheduledReqFragment extends android.support.v4.app.Fragment impl
                     i = 3;
 
                 Toast.makeText(getActivity(), "" + i, Toast.LENGTH_SHORT).show();
-                JSONObject jsonBody = new JSONObject();
-                try {
-                    jsonBody.put("Emp_QLID", "1234");
-                    jsonBody.put("Shift_ID", "4");
-                    jsonBody.put("Mgr_QLID", "456");
-                    jsonBody.put("Weekend", String.valueOf(i));
-                    jsonBody.put("Destination", dest);
-                    jsonBody.put("Reason", reasonForRequest.getText().toString());
-                    jsonBody.put("Start_Date_Time", "2018/12/12");
-                    jsonBody.put("End_Date_Time", enddate + " " + endtime + ":00");
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-                //json for request
-                JSONObject jsonBodyRequest = new JSONObject();
-                try {
-                    jsonBodyRequest.put("from", "karangupta.199317@gmail.com");
-                    jsonBodyRequest.put("recepient1", "karangupta.199317@gmail.com");
-                    jsonBodyRequest.put("recepient2", "haffiza123@gmail.com");
-                    jsonBodyRequest.put("subject", "You were expecting me");
-                    jsonBodyRequest.put("message", "this is test mail from rest api");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                JSONObject jsonBody;
 
                 // Toast.makeText(getActivity(), "" + i, Toast.LENGTH_SHORT).show();
                 if (validation()) {
@@ -152,54 +130,37 @@ public class UnscheduledReqFragment extends android.support.v4.app.Fragment impl
                     try {
                         jsonBody.put("Emp_QLID", getActivity().getSharedPreferences(null, MODE_PRIVATE).getString("Emp_qlid", "RB250491"));
                         jsonBody.put("Shift_ID", "4");
-                        jsonBody.put("Mgr_QLID", managerQLid.getText().toString());
+                        // jsonBody.put("Mgr_QLID", managerQLid.getText().toString());
                         jsonBody.put("Weekend", String.valueOf(i));
                         jsonBody.put("Destination", dest);
                         jsonBody.put("Reason", reasonForRequest.getText().toString());
-                        jsonBody.put("Start_Date_Time", "2018/12/12");
-                        jsonBody.put("End_Date_Time", enddate + " " + endtime + ":00");
+                        jsonBody.put("Start_Date_Time", fromDate);
+                        jsonBody.put("End_Date_Time", enddate);
 
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
 
 
+                    JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
 
-                    //json for request
-                    jsonBodyRequest = new JSONObject();
-                    try {
-                        jsonBodyRequest.put("from", "karangupta.199317@gmail.com");
-                        jsonBodyRequest.put("recepient1", "karangupta.199317@gmail.com");
-                        jsonBodyRequest.put("recepient2", "haffiza123@gmail.com");
-                        jsonBodyRequest.put("subject", "You were expecting me");
-                        jsonBodyRequest.put("message", "this is test mail from rest api");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                            Log.i("VOLLEY", "inside onResponse method:UnscheduledRequest");
+                            Log.i("VOLLEY", response.toString());
 
-
-                    JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST,
-                            url,
-                            jsonBody,
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-
-                                    Log.i("VOLLEY", "inside onResponse method:UnscheduledRequest");
-                                    Log.i("VOLLEY", response.toString());
-
-                                    try {
-                                        if (response.getString("status").equalsIgnoreCase("success")) {
-                                            Toast.makeText(getActivity(), "Your request is in process", Toast.LENGTH_LONG).show();
-                                        } else {
-                                            Toast.makeText(getActivity(), "Failed to make request", Toast.LENGTH_LONG).show();
-                                        }
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-
+                            try {
+                                if (response.getString("status").equalsIgnoreCase("success")) {
+                                    Toast.makeText(getActivity(), "Your request is in process", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getActivity(), "Failed to make request", Toast.LENGTH_LONG).show();
                                 }
-                            },
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    },
                             new Response.ErrorListener() {
                                 @Override
                                 public void onErrorResponse(VolleyError error) {
@@ -208,23 +169,15 @@ public class UnscheduledReqFragment extends android.support.v4.app.Fragment impl
                                     error.printStackTrace();
                                 }
                             });
-
-
-                RESTService.getInstance(getContext().getApplicationContext()).addToRequestQueue(jsonObjRequest);
+                    RESTService.getInstance(getContext().getApplicationContext()).addToRequestQueue(jsonObjRequest);
 
                     RESTService.getInstance(getContext().getApplicationContext()).addToRequestQueue(jsonObjRequest);
                 } else {
                     //  Toast.makeText(getActivity(), "Please check your entered information", Toast.LENGTH_LONG).show();
                 }
-
-
-            }
+            }//On Click listner Finish
         });
-
-
         // Toast.makeText(getActivity(), "Works!!!", Toast.LENGTH_SHORT).show();
-
-
         return rootView;
     }
 
@@ -315,6 +268,7 @@ public class UnscheduledReqFragment extends android.support.v4.app.Fragment impl
     }
 
 
+    /*Code to call the Date and time picker*/
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -327,94 +281,50 @@ public class UnscheduledReqFragment extends android.support.v4.app.Fragment impl
             case R.id.time_picker:
                 methodToSelectTime(timepicker);
                 break;
-
         }
-
     }
 
-    //method to implement functionality in Spinner
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // Toast.makeText(getActivity(), ""+position, Toast.LENGTH_SHORT).show();
-        if (position == 0) {
-            dest = "H";
-
-            displayLocationSpinner.setText("BL No:-86,Saraswati Kunj,Golf Course Road,Sector 53,Gurgaon");
-            dropLocation.setText("NCR Corporation, Vipul Plaza,Suncity,Sector 54,Gurgaon");
-        }
-        else{
-
-            displayLocationSpinner.setVisibility(View.VISIBLE);
-            displayLocationSpinner.setText(getActivity().getSharedPreferences(null, MODE_PRIVATE).getString("Emp_Address1", "BL No:-86,Saraswati Kunj,Golf Course Road,Sector 53,Gurgaon"));
-            dropLocation.setText("NCR Corporation, Vipul Plaza,Suncity,Sector 54,Gurgaon");
-
-        }
-        if (position == 2) {
-            dest = "O";
-
-
-            displayLocationSpinner.setText("NCR Corporation, Vipul Plaza,Suncity,Sector 54,Gurgaon");
-            dropLocation.setText("BL No:-86,Saraswati Kunj,Golf Course Road,Sector 53,Gurgaon");
-
-            dropLocation.setText(getActivity().getSharedPreferences(null, MODE_PRIVATE).getString("Emp_Address1", "BL No:-86,Saraswati Kunj,Golf Course Road,Sector 53,Gurgaon"));
-
-        }
-
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-
+    //method to do validation
     public Boolean validation() {
 
         if (TextUtils.isEmpty(startdate)) {
-            Snackbar snackbar = Snackbar
-                    .make(nsv, "From Date Can't be empty", Snackbar.LENGTH_LONG);
+            Snackbar snackbar = Snackbar.make(nsv, "From Date Can't be empty", Snackbar.LENGTH_LONG);
             snackbar.show();
             // fromDate.setError("Can't be empty");
         } else {
             if (TextUtils.isEmpty(enddate)) {
                 //toDate.setError("Can't be empty");
-                Snackbar snackbar = Snackbar
-                        .make(nsv, "To Date Can't be empty", Snackbar.LENGTH_LONG);
+                Snackbar snackbar = Snackbar.make(nsv, "To Date Can't be empty", Snackbar.LENGTH_LONG);
                 snackbar.show();
             } else {
                 if (TextUtils.isEmpty(endtime)) {
                     //  timepicker.setError("Can't be empty");
-                    Snackbar snackbar = Snackbar
-                            .make(nsv, "Time Can't be empty", Snackbar.LENGTH_LONG);
+                    Snackbar snackbar = Snackbar.make(nsv, "Time Can't be empty", Snackbar.LENGTH_LONG);
                     snackbar.show();
 
+//                } else {
+//                    if (TextUtils.isEmpty(managerQLid.getText().toString())) {
+//                        managerQLid.setError("Can't be empty");
+//                        Snackbar snackbar = Snackbar.make(nsv, "Manager ID Can't be empty", Snackbar.LENGTH_LONG);
+//                        snackbar.show();
                 } else {
-                    if (TextUtils.isEmpty(managerQLid.getText().toString())) {
-                        managerQLid.setError("Can't be empty");
-                        Snackbar snackbar = Snackbar
-                                .make(nsv, "Manager ID Can't be empty", Snackbar.LENGTH_LONG);
-                        snackbar.show();
-                    } else {
-                        if (isValid(managerQLid.getText().toString())) {
-                            if (TextUtils.isEmpty(reasonForRequest.getText().toString())) {
-                                reasonForRequest.setError("Can't be empty");
-                                Snackbar snackbar = Snackbar
-                                        .make(nsv, " Reason Can't be empty", Snackbar.LENGTH_LONG);
-                                snackbar.show();
-
-                            } else {
-                                return true;
-                            }
-                        } else {
-                            Snackbar snackbar = Snackbar
-                                    .make(nsv, "ID is not valid", Snackbar.LENGTH_LONG);
+                    if (true/*isValid(managerQLid.getText().toString())*/) {
+                        if (TextUtils.isEmpty(reasonForRequest.getText().toString())) {
+                            reasonForRequest.setError("Can't be empty");
+                            Snackbar snackbar = Snackbar.make(nsv, " Reason Can't be empty", Snackbar.LENGTH_LONG);
                             snackbar.show();
+
+                        } else {
+                            return true;
                         }
+                    } else {
+                        Snackbar snackbar = Snackbar.make(nsv, "ID is not valid", Snackbar.LENGTH_LONG);
+                        snackbar.show();
                     }
                 }
             }
         }
+        // }
         return false;
     }
 
@@ -435,6 +345,55 @@ public class UnscheduledReqFragment extends android.support.v4.app.Fragment impl
             }
         }
         return true;
+    }
+
+    class LocationSpinner implements AdapterView.OnItemSelectedListener{
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if(position ==1){
+                dest ="O";
+                //TODO have to get the Data from DATABASE
+                displayLocationSpinner.setText("BL No:-86,Saraswati Kunj,Golf Course Road,Sector 53,Gurgaon");
+                displayLocationSpinner.setVisibility(View.VISIBLE);
+                dropLocation.setText("NCR Corporation, Vipul Plaza,Suncity,Sector 54,Gurgaon");
+            }
+            if(position==2)
+            {
+               dest="H";
+               //TODO have to get the Data from DATABASE
+               displayLocationSpinner.setText("NCR Corporation, Vipul Plaza,Suncity,Sector 54,Gurgaon");
+                displayLocationSpinner.setVisibility(View.VISIBLE);
+               dropLocation.setText("BL No:-86,Saraswati Kunj,Golf Course Road,Sector 53,Gurgaon");
+            }
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
+    class ApprovalManagerQlid implements AdapterView.OnItemSelectedListener{
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 1){
+                    //TODO Getting the Magnager's QLid
+
+                    managerQLid_textField.setText("Mg123456");
+                }
+                if(position ==2){
+                    //TODO Getting the Magnager's QLid
+                    managerQLid_textField.setText("Mg654321");
+                }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
     }
 
 }
