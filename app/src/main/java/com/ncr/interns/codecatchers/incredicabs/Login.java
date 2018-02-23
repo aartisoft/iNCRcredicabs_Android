32a8f36dfde62a25ab90ed092e48a6a0ebcb8797
Gautineo <gauravsati1996@gmail.com>
2018-02-23 11:33:09 +0530

    Added the Alert Dialogu

--- app/src/main/java/com/ncr/interns/codecatchers/incredicabs/Login.java
+++ app/src/main/java/com/ncr/interns/codecatchers/incredicabs/Login.java
@@ -36,64 +36,66 @@ public class Login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                //json for request
-                JSONObject jsonBodyRequest = new JSONObject();
-                try {
-                    jsonBodyRequest.put("qlid", user.getText().toString());
-                    jsonBodyRequest.put("password", pass.getText().toString());
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-                JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST,
-                        url,
-                        jsonBodyRequest,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-
-                                Log.i("VOLLEY", "inside onResponse method:login");
-                                Log.i("VOLLEY", response.toString());
-
-                                try {
-                                    if (response.getString("status").equalsIgnoreCase("success")) {
-
-                                        String Emp_qlid=response.getString( "Emp_qlid");
-                                        String Mgr_Qlid=response.getString( "Mgr_Qlid");
-                                        String Emp_Address1=response.getString( "Emp_Address1");
-                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Emp_qlid",Emp_qlid).apply();
-                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Mgr_Qlid",Mgr_Qlid).apply();
-                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Emp_Address1",Emp_Address1).apply();
-                                      //  Toast.makeText(Login.this, Emp_qlid+Mgr_Qlid+Emp_Address1, Toast.LENGTH_LONG).show();
-
-                                        Intent intent = new Intent(Login.this,Dashboard.class);
-                                        startActivity(intent);
-                                    } else {
-                                        Toast.makeText(Login.this, "Invalid Credentials", Toast.LENGTH_LONG).show();
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // Do something when error occurred
-                                Log.d("VOLLEY", "Something went wrong");
-                                error.printStackTrace();
-                            }
-                        });
-
-                RESTService.getInstance(Login.this).addToRequestQueue(jsonObjRequest);
-
-            }
-        });
+//
+//
+//                //json for request
+//                JSONObject jsonBodyRequest = new JSONObject();
+//                try {
+//                    jsonBodyRequest.put("qlid", user.getText().toString());
+//                    jsonBodyRequest.put("password", pass.getText().toString());
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//
+//                JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.POST,
+//                        url,
+//                        jsonBodyRequest,
+//                        new Response.Listener<JSONObject>() {
+//                            @Override
+//                            public void onResponse(JSONObject response) {
+//
+//                                Log.i("VOLLEY", "inside onResponse method:login");
+//                                Log.i("VOLLEY", response.toString());
+//
+//                                try {
+//                                    if (response.getString("status").equalsIgnoreCase("success")) {
+//
+//                                        String Emp_qlid=response.getString( "Emp_qlid");
+//                                        String Mgr_Qlid=response.getString( "Mgr_Qlid");
+//                                        String Emp_Address1=response.getString( "Emp_Address1");
+//                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Emp_qlid",Emp_qlid).apply();
+//                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Mgr_Qlid",Mgr_Qlid).apply();
+//                                        getSharedPreferences(null,MODE_PRIVATE).edit().putString("Emp_Address1",Emp_Address1).apply();
+//                                      //  Toast.makeText(Login.this, Emp_qlid+Mgr_Qlid+Emp_Address1, Toast.LENGTH_LONG).show();
+//
+//                                        Intent intent = new Intent(Login.this,Dashboard.class);
+//                                        startActivity(intent);
+//                                    } else {
+//                                        Toast.makeText(Login.this, "Invalid Credentials", Toast.LENGTH_LONG).show();
+//                                    }
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
+//
+//                            }
+//                        },
+//                        new Response.ErrorListener() {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error) {
+//                                // Do something when error occurred
+//                                Log.d("VOLLEY", "Something went wrong");
+//                                error.printStackTrace();
+//                            }
+//                        });
+//
+//                RESTService.getInstance(Login.this).addToRequestQueue(jsonObjRequest);
+//
+//            }
+                Intent intent = new Intent(Login.this, Dashboard.class);
+                startActivity(intent);
+            }});
 
     }
 
